a977f84b6afe4d3931e8a5bdda88ec9f9e44621b
Steve Perkins <stperkins008@gmail.com>
2017-12-01 20:17:05 -0500

    updated missing code

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,10 @@ Rails.application.routes.draw do
   get 'my_portfolio', to: "users#my_portfolio"
   get 'search_stocks', to: 'stocks#search'
   get 'my_friends', to: 'users#my_friends'
+  get 'search_friends', to: 'users#search'
   resources :user_stocks, only: [:create, :destroy]
+  resources :users, only: [:show]
+  resources :friendships
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
